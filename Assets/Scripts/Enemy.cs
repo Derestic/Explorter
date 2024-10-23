55afd8be59e7307941050629f02c342e718b0c28
Derestic <alexramosbarrachina@outlook.es>
2024-10-23 18:59:09 +0200

    leve cambio

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     public GameObject Objetivo;
+    public NavMeshSurface navS;
     // Start is called before the first frame update
     void Start()
     {
