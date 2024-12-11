6bec2cb09d17a89756aee3249459e7e7a9ddb4f0
Derestic <alexramosbarrachina@outlook.es>
2024-12-11 16:57:10 +0100

    +

---
+++ Assets/Scripts/Tuto/minScriptPuerta.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minScriptPuerta : MonoBehaviour
+{
+    [SerializeField] GameObject spawn;
+    [SerializeField] GameObject panelMisionP;
+    [SerializeField] GameObject panelMisionA;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("player"))
+        {
+            other.transform.position = spawn.transform.position;
+            panelMisionP.SetActive(false);
+            panelMisionA.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/miniScript.cs
+++ Assets/Scripts/miniScript.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class miniScript : MonoBehaviour
 {
     [SerializeField] GameObject portal;
+    [SerializeField] GameObject panelMision;
+    [SerializeField] Color complete;
+    [SerializeField] Color idle;
     // Start is called before the first frame update
     void Start()
     {
-        
+        panelMision.GetComponent<Image>().color = idle;
     }
 
     // Update is called once per frame
@@ -17,6 +22,7 @@ public class miniScript : MonoBehaviour
         if (gameObject.GetComponent<npc>().isDead())
         {
             portal.SetActive(true);
+            panelMision.GetComponent<Image>().color = complete;
         }
     }
 }
