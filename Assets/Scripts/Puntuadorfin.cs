57952ecbf49b959b1b12497623c3a67319b2e380
Derestic <alexramosbarrachina@outlook.es>
2025-01-23 14:22:43 +0100

    a

--- Assets/Scripts/Puntuadorfin.cs
+++ Assets/Scripts/Puntuadorfin.cs
@@ -8,10 +8,10 @@ public class Puntuadorfin : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        MenuControl.visibilityRaton();
         TMP_Text text = transform.GetComponent<TMP_Text>();
         text.text = WaveControl.Instance().days.ToString();
         WaveControl.Instance().reset();
-        MenuControl.visibilityRaton();
     }
 
 }
